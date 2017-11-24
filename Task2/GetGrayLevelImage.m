7b5c2044f989bae5cc778e92b9f73a1f64874c96
AbdullahMahmoud <abdullahfcis@yahoo.com>
2017-11-24 20:41:25 +0200

    .

--- Task2/GetGrayLevelImage.m
+++ Task2/GetGrayLevelImage.m
@@ -2,6 +2,6 @@ function Result = GetGrayLevelImage( Image )
 %Average
 % Result = (Image(:,:,1)+Image(:,:,2)+Image(:,:,3))/3;
 %luminosity
-Result = (0.21*Image(:,:,1)+0.72*Image(:,:,2)+0.07*Image(:,:,3))/3;
+Result = (0.2989 * Image(:,:,1) + 0.5870 * Image(:,:,2) + 0.1140 * Image(:,:,3));
 end
 
